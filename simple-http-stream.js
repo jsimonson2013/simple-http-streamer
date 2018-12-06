2085f333ac1620e63fb0333fae0a2af8bb663ecf
Jacob Simonson <jsimonson2013@gmail.com>
2018-12-06 06:49:54 -0700

    Renamed. Added Comments.

--- simple-http-stream.js
+++ simple-http-stream.js
@@ -1,44 +1,72 @@
-function updatePlayer(seekTime, player, srcURL) {
-  player.innerHTML = `<source src="${srcURL}" type="audio/ogg">`
-  player.load()
-  player.play()
-  .then(player.fastSeek(seekTime))
+/*
+ * Makes GET request for ogg audio file from source. Sets playerElement's source
+ * to this file. Loads source, plays source, then seeks to seekTime.
+ */
+function loadAudioChunk(seekTime, playerElement, sourceURL) {
+  playerElement.innerHTML = `<source src="${sourceURL}" type="audio/ogg">`
+  playerElement.load()
+  playerElement.play()
+  .then(playerElement.fastSeek(seekTime))
 }
 
-function startAudioStream(player, srcURL) {
-  function streamPlayback(){
-    const seekTime = player.currentTime
-    if (seekTime >= player.duration - 3 || player.children.length < 1){
-      updatePlayer(seekTime, player, srcURL)
+/*
+ * Creates interval to play audio stream using periodic calls to load dynamic
+ * source. playerElement's source is updated and seekTime is advanced to
+ * previous position to make audio playback continuous. Source is only updated
+ * if no source or seekTime within UPDATE_PERIOD from playerElement's duration.
+ */
+function playAudioStream(playerElement, sourceURL) {
+  const UPDATE_PERIOD = 3000 // milliseconds
+
+  function updateAudioSource(){
+    const seekTime = playerElement.currentTime
+    if (seekTime >= playerElement.duration - UPDATE_PERIOD / 1000 || playerElement.children.length < 1){
+      loadAudioChunk(seekTime, playerElement, sourceURL)
     }
   }
-  interval = setInterval(streamPlayback, 3000)
+
+  const interval = setInterval(updateAudioSource, UPDATE_PERIOD)
 }
 
-function uploadRecording(audioChunk, srcURL) {
-	const blob = new Blob(['', audioChunk], {'type' : 'audio/ogg; codec: vorbis;'})
-	fetch(srcURL, {
+/*
+ * Creates new vorbis ogg Blob out of a rawAudio input and POSTs it to an
+ * uploadURL as an octet-stream.
+ */
+function uploadAudioChunk(rawAudio, uploadURL) {
+	const vorbisOggBlob = new Blob([rawAudio], {'type' : 'audio/ogg; codec: vorbis;'})
+	fetch(uploadURL, {
 		method: 'POST',
-		body: blob,
+		body: vorbisOggBlob,
 		headers:{
 			'Content-Type': 'application/octet-stream'
 		}
 	})
 }
 
-function quitRecording(quitURL) {
-	fetch(quitURL, {method: 'GET'})
+/*
+ * Calls stopCaptureURL endpoint to handle capture stop on server then reloads
+ * the page.
+ */
+function stopAudioCapture(stopCaptureURL) {
+	fetch(stopCaptureURL, {method: 'GET'})
 	.then(() => {window.open(self.location, '_self')})
 }
 
-function startAudioCapture(srcURL) {
-  const constraints = {audio: true}
+/*
+ * Requests audio capture media device. If successful, starts media recorder
+ * capturing audio every CAPTURE_PERIOD and uploads its data to uploadURL when
+ * data is available.
+ */
+function startAudioCapture(uploadURL) {
+  const CAPTURE_PERIOD = 2000 // milliseconds
+  const constraints = {audio: true} // audio resources from user media
+
 	navigator.mediaDevices.getUserMedia(constraints)
 	.then(stream => {
 		const mediaRecorder = new MediaRecorder(stream)
-		mediaRecorder.start(2000)
+		mediaRecorder.start(CAPTURE_PERIOD)
 		mediaRecorder.ondataavailable = event => {
-			uploadRecording(event.data, srcURL)
+			uploadAudioChunk(event.data, uploadURL)
 		}
 	})
 }
